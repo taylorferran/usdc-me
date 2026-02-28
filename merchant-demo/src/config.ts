0e8529f710eb6799db22534794253e601d2d0f76
taylorferran <tailzpk@hotmail.co.uk>
2026-02-28 19:35:45 +0000

    fix merchant demo

--- merchant-demo/src/config.ts
+++ merchant-demo/src/config.ts
@@ -3,5 +3,5 @@
 
 export const MERCHANT_API_KEY = 'usdcme_bc15f21493f87e6672422eee9f55d68e8db5513dfb4c9e9f056f992bb4de461b';
 
-export const API_URL = 'http://localhost:3001/api';
-export const FRONTEND_URL = 'http://localhost:3000';
+export const API_URL = import.meta.env.VITE_API_URL || 'https://usdc-me.vercel.app/api';
+export const FRONTEND_URL = import.meta.env.VITE_FRONTEND_URL || 'https://usdc-me.vercel.app';
